b3b68b2f391bc55b1de16c4f34431d2645f534d9
saveliimorozov <savkamorozov7@gmail.com>
2023-03-03 18:08:44 +0300

    Added dateAdd, price and techInfo

--- main.py
+++ main.py
@@ -7,19 +7,23 @@ from bs4 import BeautifulSoup as bs
 import pandas as pd
 import random
 
-
 url = 'https://ss.ge/ru/%D0%BD%D0%B5%D0%B4%D0%B2%D0%B8%D0%B6%D0%B8%D0%BC%D0%BE%D1%81%D1%82%D1%8C/l/%D0%9A%D0%B2%D0%B0%D1%80%D1%82%D0%B8%D1%80%D0%B0/%D0%90%D1%80%D0%B5%D0%BD%D0%B4%D0%B0?Page=1&RealEstateTypeId=5&RealEstateDealTypeId=1&Sort.SortExpression=%22OrderDate%22%20DESC&MunicipalityId=95&CityIdList=95&PrcSource=2&CommercialRealEstateType=&PriceType=false&CurrencyId=2&PriceTo=500&Context.Request.Query[Query]=&WithImageOnly=true'
 
-def getSitePageInText(url: str):
 
+def getSitePageInText(url: str):
     urlReq = req.get(url,
                      cookies=rc.cookies,
                      headers=rc.headers)
 
     time.sleep(1)
     print(urlReq)
-    time.sleep(2)
+    time.sleep(1)
 
+    with open(r'C:\Users\morozsa\PycharmProjects\flatsadsparser\requestPage.html', 'w', encoding='utf-8') as file:
+        file.write(urlReq.text)
+
+    # with open(r'C:\Users\morozsa\PycharmProjects\flatsadsparser\requestPage.html', 'r', encoding='utf-8') as file:
+    #     urlReq = file.read()
     soupUrlReq = bs(urlReq.text, 'lxml')
     time.sleep(1)
     # print(soupUrlReq)
@@ -30,9 +34,8 @@ def getSitePageInText(url: str):
 def getAdsList(soupUrlReq):
     adsMainList = set(soupUrlReq.findAll('div', class_="latest_article_each"))
     adsMainList.update(set(soupUrlReq.findAll('div', class_="latest_article_each latest_article_each_coloured")))
-    time.sleep(4)
+    time.sleep(2)
     return adsMainList
-
 
 
 def getAdsMainInfo(singleAdText):
@@ -42,33 +45,33 @@ def getAdsMainInfo(singleAdText):
 
     if adMainInfo:
         try:
-            adLink = 'https://www.ss.ge' + adMainInfo.find('div', class_ = 'latest_desc').find('a').get('href')
+            adLink = 'https://www.ss.ge' + adMainInfo.find('div', class_='latest_desc').find('a').get('href')
 
             id = re.search(r'[-]\d{7}', adLink)[0][1:]
 
-            title = adMainInfo.find('span', class_ = 'TiTleSpanList').text
-            # movieNameRu = adMainInfo.find('div', class_='base-movie-main-info_mainInfo__ZL_u3').find(
-            #     'span', class_='styles_mainTitle__IFQyZ styles_activeMovieTittle__kJdJj').text
-            #
-            # movieNameOrig = adMainInfo.find('div', class_='desktop-list-main-info_secondaryTitleSlot__mc0mI').find(
-            #     'span', class_='desktop-list-main-info_secondaryTitle__ighTt')
-            # if movieNameOrig != None:
-            #     movieNameOrig = movieNameOrig.text
-            #     movieYearandDuration = adMainInfo.find(
-            #         'span', class_="desktop-list-main-info_secondaryText__M_aus").contents[2]
-            # else:
-            #     movieNameOrig = movieNameRu
-            #     movieYearandDuration = adMainInfo.find(
-            #         'span', class_="desktop-list-main-info_secondaryText__M_aus").contents[0]
-            #
-            # movieCountryTypeDirector = adMainInfo.find(
-            #     'span', class_="desktop-list-main-info_truncatedText__IMQRP").contents[0]
+            title = adMainInfo.find('span', class_='TiTleSpanList').text
+
+            techInfo = [re.sub(r'[\n\r\s]',"",adMainInfo.find('div', class_ = 'latest_flat_km').text),
+                        re.sub(r'[\n\r\s]',"",adMainInfo.find('div', class_='latest_flat_type').text),
+                        re.sub(r'[\n\r\s]',"",adMainInfo.find('div', class_='latest_stair_count').text)
+            ]
+
+            addTime = adMainInfo.find('div', class_ = 'add_time').text.strip()
+
+            price = re.sub(r'[\n\r\s]',"",adMainInfo.find('div', class_='price-spot dalla').find('div', class_='latest_price').text)
+
+
+
+
 
             singleAdDict = {
-                               'Link': adLink,
-                                'Id' : id,
-                                'Title' : title
-                               }
+                'Link': adLink,
+                'Id': id,
+                'Title': title,
+                'TechInfo' : techInfo,
+                'AddTime' : addTime,
+                'Price' : price
+            }
         except Exception as err:
             print(err)
             print('Error single dict')
@@ -78,19 +81,25 @@ def getAdsMainInfo(singleAdText):
     return singleAdDict
 
 
-
-
 if __name__ == "__main__":
+
     AdsList = getAdsList(getSitePageInText(url))
     print(len(AdsList))
-    print(list(AdsList)[:3])
-    listAdsDicts = [getAdsMainInfo(ad) for ad in AdsList]
-    print(listAdsDicts)
-    print(len(listAdsDicts))
-    with open(r'C:\Users\morozsa\PycharmProjects\flatsadsparser\urlsExample.txt', 'w') as file:
-        for adDict in listAdsDicts:
-            file.write(adDict['Link'] + '\n')
+
+    sortedListAdsDicts = sorted([getAdsMainInfo(ad) for ad in AdsList], key= lambda adDict: adDict['AddTime'], reverse=True)
+
+
+
+
+
+    with open(r'C:\Users\morozsa\PycharmProjects\flatsadsparser\urlsExample.txt', 'w', encoding='utf-8') as file:
+        for adDict in sortedListAdsDicts:
             file.write(adDict['Id'] + '\n')
-            file.write(adDict['Title'] + '\n\n')
+            file.write(adDict['Link'] + '\n')
+            file.write(adDict['Title'] + '\n')
+            file.write(" ".join(adDict['TechInfo']) + '\n')
+            file.write(adDict['Price'] + '\n')
+            file.write(adDict['AddTime'] + '\n\n')
+
 
 
